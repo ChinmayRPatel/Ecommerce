f3ffbadc5f12afed1067cbbe9b439687fca8054b
ChinmayRPatel <chinmayrpatel1309@gmail.com>
2024-06-17 18:13:31 +0530

    Initial commit

---
+++ src/main/java/com/ecom/Ecommerce/config/MyConfig.java
@@ -0,0 +1,14 @@
+package com.ecom.Ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class MyConfig {
+
+	@Bean
+	BCryptPasswordEncoder getEnconder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ src/main/java/com/ecom/Ecommerce/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.ecom.Ecommerce.dao;
+
+import com.ecom.Ecommerce.bean.UserBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+//Controller 
+//Repository 
+//Service 
+//Component 
+
+@Repository
+public class UserDao {
+	@Autowired
+	JdbcTemplate stmt;
+
+	public void insertUser(UserBean user) {
+		user.setRole("CUSTOMER");
+		stmt.update("insert into users (firstName,gender,email,password,role) values (?,?,?,?,?)", user.getFirstName(),
+				user.getGender(), user.getEmail(), user.getPassword(), user.getRole());
+	}
+
+	public UserBean getUserByEmail(String email) {
+		UserBean user = stmt.queryForObject("select * from users where email = ? ",
+				new BeanPropertyRowMapper<>(UserBean.class), new Object[] { email });
+		return user; 
+	}
+}
